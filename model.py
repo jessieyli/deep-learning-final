189af726b3e40b687c8eb03d615ef03e927dec14
jessieyli <50643746+jessieyli@users.noreply.github.com>
2020-12-08 01:49:30 -0800

    Update model.py
    Added generator

--- model.py
+++ model.py
@@ -56,22 +56,86 @@ class VideoGAN(tf.keras.Model):
 
 
 class Generator(tf.keras.Model):
-    def __init__(self, latent_dim):
+    def __init__(self, z_dim):
         """
         The Generator class that inherits from tf.keras.Model
         """
         super(Generator, self).__init__()
-        self.latent_dim = latent_dim
+        self.z_dim = z_dim # most likely 100
+        # background stream 2D convolutions
+        # 1x1x100 -> 4x4x512
+        self.deconv1 = tf.keras.layers.Conv2DTranspose(512, kernel_size=(4, 4), strides=(1,1), padding='valid', activation='relu')
+        # 8x8x256
+        self.deconv2 = tf.keras.layers.Conv2DTranspose(256, kernel_size=(4, 4), strides=(2,2), padding='same', activation='relu')
+        # 16x16x128
+        self.deconv3 = tf.keras.layers.Conv2DTranspose(128, kernel_size=(4, 4), strides=(2,2), padding='same', activation='relu')
+        # 32x32x64
+        self.deconv4 = tf.keras.layers.Conv2DTranspose(64, kernel_size=(4, 4), strides=(2,2), padding='same', activation='relu')
+        # 64x64x3
+        self.deconv5 = tf.keras.layers.Conv2DTranspose(3, kernel_size=(4, 4), strides=(2,2), padding='same', activation='tanh')
 
-    def call(self, inputs):
+        # foreground stream 3D convolutions
+        # [4x4x4] convolutions with a stride of 2 except for the first layer
+        # 1x1x1x100 -> 4x4x2x512
+        self.deconvfg1 = tf.keras.layers.Conv3DTranspose(512, kernel_size=(2, 4, 4), strides=(1,1,1), padding='valid', activation='relu')
+        # 8x8x4x256
+        self.deconvfg2 = tf.keras.layers.Conv3DTranspose(256, kernel_size=(4, 4, 4), strides=(2,2,2), padding='same', activation='relu')
+        # 16x16x8x128
+        self.deconvfg3 = tf.keras.layers.Conv3DTranspose(128, kernel_size=(4, 4, 4), strides=(2,2,2), padding='same', activation='relu')
+        # 32x32x16x64
+        self.deconvfg4 = tf.keras.layers.Conv3DTranspose(64, kernel_size=(4, 4, 4), strides=(2,2,2), padding='same', activation='relu')
+        # 64x64x32x3
+        self.deconvfg5 = tf.keras.layers.Conv3DTranspose(3, kernel_size=(4, 4, 4), strides=(2,2,2), padding='same', activation='tanh')
+        
+        # generate mask with sigmoid function
+        self.deconv_mask = tf.keras.layers.Conv3DTranspose(1, kernel_size=(4, 4, 4), strides=(2,2,2), padding='same', activation='sigmoid')
+
+
+    def call(self, z):
         """
         Executes the generator model on the random noise vectors.
         
-        :param inputs: Noise vector with shape (batch_size, 100)
-        :return: Generated video with shape (batch_size, 64, 64, 32, 3), i.e. batch_size x height x width x frames x color channels
+        :param z: latent representation, noise vector with shape (batch_size, 100)
+        :return: Generated video with shape (batch_size, 32, 64, 64, 3), i.e. batch_size x height x width x frames x color channels
         """
-        # TO-DO: Implement two-stream fracionally-strided spatio-temporal convolutions with masking
-        return 
+        # TO-DO: Implement two-stream fractionally-strided spatio-temporal convolutions with masking
+        # mask selects either the foreground or background for each pixel location and time
+        # generator architecture https://arxiv.org/pdf/1511.06434.pdf
+        # tips from above: use relu activation for all layers except for the output, which is tanh
+        # use batchnorm
+
+        # create background stream
+        batch_size = z.shape[0]
+        z_bg = tf.reshape(z, [batch_size, 1, 1, self.z_dim])
+        dc1 = self.deconv1(z_bg) # [bs, 4, 4, 512]
+        dc2 = self.deconv2(dc1) # [bs, 8, 8, 256]
+        dc3 = self.deconv3(dc2)
+        dc4 = self.deconv4(dc3)
+        dc5 = self.deconv5(dc4)
+        background = dc5 # [bs, 64, 64, 3]
+
+        # create foreground stream
+        z_fg = tf.reshape(z, [batch_size, 1, 1, 1, self.z_dim])
+        dc_fg1 = self.deconvfg1(z_fg) # [bs, 2, 4, 4, 512]
+        dc_fg2 = self.deconvfg2(dc_fg1)
+        dc_fg3 = self.deconvfg3(dc_fg2)
+        dc_fg4 = self.deconvfg4(dc_fg3)
+        dc_fg5 = self.deconvfg5(dc_fg4)
+        foreground = dc_fg5 # [bs, 32, 64, 64, 3]
+
+        # create mask
+        mask = self.deconv_mask(dc_fg4) # [bs, 32, 64, 64, 1]
+
+        # replicate background and mask
+        # "need to replicate singleton dimensions to match corresponding tensor"
+        background = tf.expand_dims(background, 1) # [bs,64,64,3] -> [bs,1,64,64,3]
+        background = tf.tile(background, [1, 32, 1, 1, 1])
+        mask = tf.tile(mask, [1, 1, 1, 1, 3])
+
+        # incorporate mask to generate video
+        # m * f + (1-m) * b where * is element-wise multiplication
+        video = tf.add(tf.multiply(mask, foreground), tf.multiply(1-mask, background))
+        return video # [bs, 32, 64, 64, 3]
 
 
 class Discriminator(tf.keras.Model):
@@ -140,3 +204,8 @@ def d_wasserstein_loss(fake_preds, real_preds):
 def g_wasserstein_loss(fake_preds):
     return tf.reduce_sum(fake_preds)
 
+# batch_size = 10
+# z_input = np.random.normal(size=(batch_size, 100))
+# z = tf.convert_to_tensor(z_input)
+# gen = Generator(100)
+# gen.call(z)
